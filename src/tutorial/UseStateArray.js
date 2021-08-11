1dbe406b7d4115fd251c40d64e6f69f60c2b0b85
Gaurav <gaurav.jain2@soprasteria.com>
2021-08-11 10:15:56 +0530

    How to remove function individually

--- src/tutorial/UseStateArray.js
+++ src/tutorial/UseStateArray.js
@@ -4,6 +4,11 @@ import { data } from "./data";
 function UseStateArray() {
 
     const [people, setPeople] = React.useState(data)  // another way to write use state like 
+    const removeItem = (id) => {
+        let newPeople = people.filter((person) => person.id !== id);
+        setPeople(newPeople);
+    }
+   
     return (
         <>
         {
@@ -13,6 +18,7 @@ function UseStateArray() {
              return (
                  <div key={id} className = 'item'>
                      <h4>{name}</h4>
+                     <button onClick={() =>removeItem(id)}> remove him </button>
                  </div>
              )
           }
