7306a5c23877d175959bfc0e9e4ce372855ee5ba
Gaurav <gaurav.jain2@soprasteria.com>
2021-08-11 11:50:57 +0530

    How to Increase the Counter

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
 //import UseStateObject from "./tutorial/useStateObject";
 import UseStateBasic from "./tutorial/UseStateBasic";
-import UseStateArray  from "./tutorial/UseStateArray";
+import UseStateArray from "./tutorial/UseStateArray";
 import UseStateObjects from './tutorial/UseStateObjects';
+import UseStateCounter from './tutorial/UseStateCounter';
 
 
 function App() {
-  
-  return (<div className = 'container'>
-     <h2> Advance React js</h2>
-     
-     <UseStateObjects/>
-     </div>
-    );
+
+  return (<div className='container'>
+    <h2> Advance React js</h2>
+
+    <UseStateCounter />
+  </div>
+  );
 }
 
 export default App;
