02e0026fe9268713b20cb08607504d3c47916b6d
Gaurav <gaurav.jain2@soprasteria.com>
2021-08-11 17:13:05 +0530

    Use Handling

--- src/tutorial/Forms.js
+++ src/tutorial/Forms.js
@@ -9,30 +9,56 @@ import React, { useState } from 'react'
 
 function Forms() {
 
-    const [firstName, setFirstName] = useState("");
-    const [email, setEmail] = useState("");
+    const [firstName, setFirstName] = useState(" ");
+    const [email, setEmail] = useState(" ");
+    const [people, setPeople] = useState([]);
 
-
-    const handleSubmit = (e) =>{
+    const handleSubmit = (e) => {
         e.preventDefault(); // this is used to prevent our default behaviour of our browser
-        console.log("Hello my Name is Gaurav Jain")
+        if (firstName && email) {
+            console.log('sumbit the value');
+            const person = { id: new Date().getTime.toString, firstName, email };
+            console.log(person);
+            setPeople((people) => {
+
+                return [...people, person];
+            });
+            setFirstName('');
+            setEmail('');
+
+        }
+        else {
+            console.log('empty value');
+        }
     }
     return (
         <>
             <article>
-                <form className='form' onSubmit = {handleSubmit}>
+                <form className='form' onSubmit={handleSubmit}>
                     <div className='form-control'>
                         <label htmlFor="firstName"> Name :</label>
-                        <input type="text" id="firstName" name='firstname'></input>
+                        <input type="text" id="firstName" name='firstname' value={firstName} onChange={(e) => setFirstName(e.target.value)}></input>
                     </div>
 
                     <div className='form-control'>
                         <label htmlFor="email"> Email :</label>
-                        <input type="text" id="email" name='email'></input>
+                        <input type="text" id="email" name='email' value={email} onChange={(e) => setEmail(e.target.value)}></input>
                     </div>
 
                     <button type="sumbit">Sumbit</button>
                 </form>
+
+                {people.map((person, index) => {
+
+                   const {id, firstName, email} = person;
+                   return(
+                   <div className = "item" key = {id}> {/* udate*/}
+                       <h4>{firstName}</h4>
+                       <p>{email}</p>
+                   </div>
+                   );
+                })}
+
 
             </article>
         </>
