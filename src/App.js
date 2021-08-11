1a1728efa493e9411c9333f9e433ab3ff53a67d3
Gaurav <gaurav.jain2@soprasteria.com>
2021-08-11 10:35:18 +0530

    Use State Function

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 //import UseStateObject from "./tutorial/useStateObject";
 import UseStateBasic from "./tutorial/UseStateBasic";
 import UseStateArray  from "./tutorial/UseStateArray";
+import UseStateObjects from './tutorial/UseStateObjects';
+
 
 function App() {
   
   return (<div className = 'container'>
      <h2> Advance React js</h2>
-     <UseStateArray/>
+     
+     <UseStateObjects/>
      </div>
     );
 }
---
+++ src/tutorial/UseStateObjects.js
@@ -0,0 +1,30 @@
+import React, {useState} from 'react'
+
+function UseStateObjects() {
+
+    const [person, setPerson] = useState({
+
+        name: "Start Lord",
+        age: 50,
+        message: "I am god",
+    });
+
+    const changeMessage = () => {
+
+        setPerson("Not anymore")
+    }
+    return (
+        <div>
+           <>
+              <h3>{person.name}</h3>
+              <h3>{person.age}</h3>
+              <h3>{person.message}</h3>
+
+              <button className = "btn" onClick={changeMessage}>Change my message </button>
+           </>
+
+        </div>
+    )
+}
+
+export default UseStateObjects
