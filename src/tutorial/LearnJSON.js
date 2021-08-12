d4f7adb66338fb169b3530d0622cac8122a8457b
Gaurav <gaurav.jain2@soprasteria.com>
2021-08-12 10:43:06 +0530

    Learn how to push data to JSON server

--- src/tutorial/LearnJSON.js
+++ src/tutorial/LearnJSON.js
@@ -3,13 +3,30 @@ import axios from 'axios'
 
 function LearnJSON() {
 
-    const handleClick = (b) =>{
-        const promise = axios.post("http://localhost:4000/users")
-    }
+    const handleClick = (b) => {
+        const promise = axios.post("http://localhost:4000/users",{  // initalize the json server
+          name: "Tony"},{
+              headers: {
+               ['content-type']: 'application/json'
+           
+            }}
+        )
+            promise.then(response => {            // data connection establishment and response from the servor   
+            console.log(response);
+         })
+            promise.catch(error=>{
+            console.error(error);
+         })
+
+         let c = b;
+         console.log(b);
+     }
+
     return (
         <>
-        <button type = "button" className='btn' onClick={handleClick}> send User</button>
-        <button type = "button" className='btn' onClick={getUsers}> get Users </button>
+            <h2> Random Title </h2>
+            <button type="button" className='btn' onClick={handleClick}> post users </button>
+           {/* <button type="button" className='btn' onClick={getUsers}> get users </button> */}
         </>
     )
 }
