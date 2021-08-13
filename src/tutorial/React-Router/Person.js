8da02224ef8d269a5c8779ac12619024e1a0090c
Gaurav <gaurav.jain2@soprasteria.com>
2021-08-13 11:52:53 +0530

    Learn How to use UseParams (Feature of Hook )

--- src/tutorial/React-Router/Person.js
+++ src/tutorial/React-Router/Person.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import {data} from '../data'
+import {Link, useParams} from "react-router-dom";
+
 
 function Person() {
-    return (
+
+   console.log(useParams());  // Here usedParams is used to render the id of an object that we have created 
+   const [name,setName] = useState("Default State")
+   const {id} = useParams();
+   useEffect(() =>
+   {
+       const  newPerson = data.find((person) => person.id === parseInt(id));
+       setName(newPerson.name)
+
+   },[]) 
+   
+   return (
+        
+        <>
         <div>
-            
+            <h1>{name}</h1>
+            <Link to = "/people" className="btn"> 
+            Back To People 
+            </Link>
+        
         </div>
+        </>
     )
 }
 
