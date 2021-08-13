b9401f22be31f2906797cbee63881942471eb41d
Gaurav <gaurav.jain2@soprasteria.com>
2021-08-13 12:50:51 +0530

    Pr

--- src/tutorial/Prop-Drilling.js
+++ src/tutorial/Prop-Drilling.js
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {data} from "./data"
 
 function PropDrilling() {
+
+    const [people, setPeople] = useState(data);
+    const removePerson = (id) =>
+    {
+        setPeople((people) => 
+        {
+            return people.filter((person) => person.id !== id)
+        })
+    }
     return (
-        <div>
-            
-        </div>
+        <>
+         <section>
+            <h3> Prop drilling </h3>
+            <List people= {people} removePerson={removePerson}/>   
+        </section>   
+        </>
     )
+}
+
+const List = ({people, removePerson}) => {
+    return <>
+    {
+        people.map((person) => {
+           return <SinglePerson key={person.id} {...person}
+           removePerson ={removePerson}/>;
+        })
+    }
+    </>
+}
+
+const SinglePerson = ({id,name, removePerson}) => {
+    return <div className ="item">
+      <h4>{name}</h4> 
+      <button onClick= {() => removePerson(id)}>remove</button>
+    </div>
 }
 
 export default PropDrilling
